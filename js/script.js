e33ba7d38ae6185b7e4539847905de0e36c66941
Habib <70913270+HabibBD99@users.noreply.github.com>
2024-11-10 21:59:52 +0600

    robot speak code added

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function MyRobot()
   --------------------------------*/
  var RobotDataStore = [
     {
-      'name'         : 'Hi I am Robot. My name is Kori Uddin.',
+      'name'         : 'Hi I am Robot. My name is Korim Uddin.',
       'age'          : 'Birth 10 January 2002 Year. That Present 22 Year Old.',
       'work'         : 'I am working with Holy Quran.',
       'discover'     : 'This Robot is Discover 30-11-2024 by The Habib Engineer.',
@@ -64,8 +64,26 @@ function MyRobot()
  
               }
           } 
+
+
 */
-   
+
+
+/*
+    var speaker = new SpeechSynthesisUtterance();
+                speaker.lang = "En-United-Us";
+                speaker.text = SpeechValue;
+                speaker.volume = 10;
+                speaker.rate = 1;
+                speaker.pitch = 2;
+    window.speechSynthesis.speak(speaker);   
+*/
+
+
+
+   /*------------------------------------
+      Robot Data Match And Speek Work
+    -------------------------------------*/
 
    if(searchText === '')
    {
@@ -74,40 +92,100 @@ function MyRobot()
    }
    else if (searchText == "name" || searchText == "what your name")
    {
-     textShow.innerText = RobotDataStore[0].name;   
+    textShow.innerText = RobotDataStore[0].name; 
+       const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].name;
+             SpeakSound.volume = 10;
+             SpeakSound.rate = 1;
+             SpeakSound.pitch = 2;
+       const voices = speechSynthesis.getVoices()
+             SpeakSound.voice = voices[0];
+      window.speechSynthesis.speak(SpeakSound);  
    }
+
    else if (searchText == "work" || searchText == "what you do")
    {
-     textShow.innerText = RobotDataStore[0].work;   
+     textShow.innerText = RobotDataStore[0].work;
+     var Speaker = new SpeechSynthesisUtterance();
+                Speaker.lang = "En-United-Us";
+                Speaker.text = RobotDataStore[0].work;
+                Speaker.volume = 10; 
+     window.speechSynthesis.speak(Speaker);  
    }
    else if (searchText == "discover" || searchText == "invented" || searchText == "who is the discover man")
    {
-     textShow.innerText = RobotDataStore[0].discover;   
+     textShow.innerText = RobotDataStore[0].discover;
+       const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].discover;
+             SpeakSound.volume = 10;
+       const voices = speechSynthesis.getVoices()
+             SpeakSound.voice = voices[1];
+      window.speechSynthesis.speak(SpeakSound);    
    }
    else if (searchText == "ok")
    {
-     textShow.innerText = RobotDataStore[0].ok;   
+     textShow.innerText = RobotDataStore[0].ok;
+        const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].ok;
+             SpeakSound.volume = 10;
+        const voices = speechSynthesis.getVoices()
+            SpeakSound.voice = voices[1];
+      window.speechSynthesis.speak(SpeakSound); 
+
    }
    else if (searchText == "good bye")
    {
-     textShow.innerText = RobotDataStore[0].goodbye;   
+     textShow.innerText = RobotDataStore[0].goodbye;
+        const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].goodbye;
+             SpeakSound.volume = 10;
+        const voices = speechSynthesis.getVoices()
+             SpeakSound.voice = voices[1];
+      window.speechSynthesis.speak(SpeakSound);    
    }
    else if (searchText == "assalamualaikum")
    {
-     textShow.innerText = RobotDataStore[0].salamReply;   
+     textShow.innerText = RobotDataStore[0].salamReply;
+        const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].salamReply;
+             SpeakSound.volume = 10;
+        const voices = speechSynthesis.getVoices()
+             SpeakSound.voice = voices[1];
+      window.speechSynthesis.speak(SpeakSound);    
    }
     else if (searchText == "age")
    {
-     textShow.innerText = RobotDataStore[0].age;   
+     textShow.innerText = RobotDataStore[0].age;
+        const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].age;
+             SpeakSound.volume = 10;
+        const voices = speechSynthesis.getVoices()
+             SpeakSound.voice = voices[0];
+      window.speechSynthesis.speak(SpeakSound);    
    } 
     else if (searchText == "hi" || searchText == "hello")
    {
-     textShow.innerText = RobotDataStore[0].hello;   
+     textShow.innerText = RobotDataStore[0].hello;
+        const SpeakSound = new SpeechSynthesisUtterance();
+             SpeakSound.lang = "En-United-Kingdom";
+             SpeakSound.text = RobotDataStore[0].hello;
+             SpeakSound.volume = 10;
+        const voices = speechSynthesis.getVoices()
+             SpeakSound.voice = voices[3];
+      window.speechSynthesis.speak(SpeakSound);    
    }
         
    else{  
       textShow.innerHTML = "<P style='color: red;'>The Value is Not Matching.</p>";
    }
+
+
    
 }
 
@@ -119,6 +197,11 @@ function MyRobot()
    var clrText = document.getElementById("search_text");
    var textShow = document.getElementById('Text_Show').innerText='';
  }
+
+/*-------------...Scroll Button End...----------------*/
+
+
+
 
 
 
@@ -153,3 +236,9 @@ function MyRobot()
     }
 
 /*-------------...Scroll Button End...----------------*/
+
+
+
+
+
+
