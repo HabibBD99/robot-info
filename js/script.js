1d33c3533b31de16c6a604eb395508b3d8cac3a0
HabibBD99 <habibsparknet@gmail.com>
2024-11-10 14:31:12 +0600

    display clear code added

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ function MyRobot()
  var searchText = document.getElementById('search_text').value;
    var textShow = document.getElementById('Text_Show');
 
- /*------------------------
-    Storing Data Of Robot
-  -------------------------*/
- var dataStore = [
+ /*-------------------------------
+    Storing Data Of Robot Answer
+  --------------------------------*/
+ var RobotDataStore = [
     {
       'name'         : 'Hi I am Robot. My name is Kori Uddin.',
       'age'          : 'Birth 10 January 2002 Year. That Present 22 Year Old.',
@@ -15,7 +15,7 @@ function MyRobot()
       'discover'     : 'This Robot is Discover 30-11-2024 by The Habib Engineer.',
       'goodbye'      : 'Ok Thanks.',
       'ok'           : 'Good Bye Thanks.',
-      'hello'       : 'You Can Tell.',
+      'hello'        : 'You Can Tell.',
       'salam'        : 'Assalamualaikum',
       'salamReply'   : 'Oyalaikumussalam Oyarahmatullahy Oyabarkatu.\n\n Assalamualaikum.?',
 
@@ -26,57 +26,100 @@ function MyRobot()
  /*------------------------...End...-------------------------*/
 
 
+ /*--------------------------
+     Robot Question Match
+  ---------------------------*/
+ var RobotQuestionMatch = [
+      'name',
+      'what is your name',
+      'work',
+      'what you do'
 
+  ];
 
-//document.write("<br/> <a href='index.html' class='Back'>back</a>");
+ /*------------------------...End...-------------------------*/
+
 
  /*---------------------------------------
-     Validation And Matching Data Display
+    Validation And Matching Data Display
   ----------------------------------------*/
+/*
+   if(searchText === '')
+   {
+     alert('Please Valid Keyword Search.');
+     return false;
+   }    
+
+   for (var i=0; i<RobotDataStore.length; i++) 
+   {     
+          
+          for (var j=0; j<RobotQuestionMatch.length; j++) 
+          {
+              if(searchText == RobotQuestionMatch[j])
+              {
+                textShow.innerText = RobotDataStore[0][j].name;   
+              }
+              else{
+                textShow.innerHTML = "<P style='color: red;'>The Value is Not Matching.</p>";
+ 
+              }
+          } 
+*/
+   
+
    if(searchText === '')
    {
      alert('Please Valid Keyword Search.');
      return false;
    }
-   else if (searchText == "name" || searchText == "what is your name") 
+   else if (searchText == "name" || searchText == "what your name")
    {
-     textShow.innerText = dataStore[0].name;
+     textShow.innerText = RobotDataStore[0].name;   
    }
    else if (searchText == "work" || searchText == "what you do")
    {
-     textShow.innerText = dataStore[0].work;   
+     textShow.innerText = RobotDataStore[0].work;   
    }
    else if (searchText == "discover" || searchText == "invented" || searchText == "who is the discover man")
    {
-     textShow.innerText = dataStore[0].discover;   
+     textShow.innerText = RobotDataStore[0].discover;   
    }
    else if (searchText == "ok")
    {
-     textShow.innerText = dataStore[0].ok;   
+     textShow.innerText = RobotDataStore[0].ok;   
    }
    else if (searchText == "good bye")
    {
-     textShow.innerText = dataStore[0].goodbye;   
+     textShow.innerText = RobotDataStore[0].goodbye;   
    }
    else if (searchText == "assalamualaikum")
    {
-     textShow.innerText = dataStore[0].salamReply;   
+     textShow.innerText = RobotDataStore[0].salamReply;   
    }
     else if (searchText == "age")
    {
-     textShow.innerText = dataStore[0].age;   
+     textShow.innerText = RobotDataStore[0].age;   
    } 
     else if (searchText == "hi" || searchText == "hello")
    {
-     textShow.innerText = dataStore[0].hello;   
-   }         
+     textShow.innerText = RobotDataStore[0].hello;   
+   }
+        
    else{  
       textShow.innerHTML = "<P style='color: red;'>The Value is Not Matching.</p>";
    }
-
-
-
+   
 }
+
+
+/*------------------------------------
+      Clearing Display Value Work
+ -------------------------------------*/
+ function TextClear() {
+   var clrText = document.getElementById("search_text");
+   var textShow = document.getElementById('Text_Show').innerText='';
+ }
+
 
 
 /*------------------------------------
